52fb52eca101814ab3811ece5f14e05476f5893f
Esteban Lucchesi <lucchesi.esteban@gmail.com>
2014-06-04 17:58:29 -0300

    Initial commit

---
+++ gen/org/esteban/piano/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.esteban.piano;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/esteban/piano/MainActivity.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of Piano.
+ * 
+ * Piano is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Piano is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Piano.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * Piano
+ * Virtual piano based on Hexiano (https://gitorious.org/hexiano)
+ * Original sounds from http://theremin.music.uiowa.edu/MISpiano.html
+ */
+
+package org.esteban.piano;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+	// Custom view
+	PianoLayout pianoView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Show the view in full screen mode without title
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// Always portrait orientation
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		// Set view
+		pianoView = new PianoLayout(this.getApplicationContext());
+		setContentView(pianoView);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		
+		pianoView.destroy();
+	}
+}
